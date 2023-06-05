f95d0785c1f09c7d98f9183a91a925969f7ffd46
dnsv7 <vodolazhchenko29@gmail.com>
2023-06-05 21:21:15 +0300

    obj and function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
-let login = prompt("Введіть логін")
+// 1
 
-if (login === "Admin") {
-  const password = prompt("Введіть пароль")
-  if (password === "12345") {
-    alert("Ласкаво просимо")
-  } else {
-    alert("Невірний пароль")
+// let login = prompt("Введіть логін")
+
+// if (login === "Admin") {
+//   const password = prompt("Введіть пароль")
+//   if (password === "12345") {
+//     alert("Ласкаво просимо")
+//   } else {
+//     alert("Невірний пароль")
+//   }
+// } else {
+//   alert("Доступ заборонено")
+// }
+
+// 2
+
+// 2.1
+
+const tour = {
+  name: "Trip to Kharkiv",
+  description: "description",
+  price: 300,
+  duration: "24 hours",
+  displayInfo: function () {
+    console.log(this.name, this.description, this.price, this.duration)
   }
-} else {
-  alert("Доступ заборонено")
 }
+
+tour.displayInfo()
+
+// 2.2
+
+function calculateTotalCost(obj, number) {
+  console.log(obj.price * number);
+}
+
+calculateTotalCost(tour, 100)
